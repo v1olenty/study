4a70519366f1c5270e9b594e66e265ee62b1eb91
v1olenty <v1ol3nty@gmail.com>
2025-07-25 05:42:50 +0300

    Update biology.py

--- biology.py
+++ biology.py
@@ -4,5 +4,5 @@ stage2 = input("Второй этап: ")  # Например, "Homo habilis"
 stage3 = input("Третий этап: ")  # Например, "Homo erectus"
 stage4 = input("Четвертый этап: ")  # Например, "Homo sapiens sapiens"
 
-# Выводим результат с разделителем =>
+# Выводим результат с разделителем +
 print(stage1, stage2, stage3, stage4, sep=' + ')
