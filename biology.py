d159176fe037d2a3bd2cf615130e3743d32c57ee
v1olenty <v1ol3nty@gmail.com>
2025-07-25 05:40:20 +0300

    первый урок

---
+++ biology.py
@@ -0,0 +1,8 @@
+# Запрашиваем этапы развития
+stage1 = input("Первый этап: ")  # Например, "Австралопитек"
+stage2 = input("Второй этап: ")  # Например, "Homo habilis"
+stage3 = input("Третий этап: ")  # Например, "Homo erectus"
+stage4 = input("Четвертый этап: ")  # Например, "Homo sapiens sapiens"
+
+# Выводим результат с разделителем =>
+print(stage1, stage2, stage3, stage4, sep=' + ')
